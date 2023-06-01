dc9b4c3f1ebf23194419e20d72e7f4dc0344908d
Yash Khare <yashk@dewsolutions.in>
2023-06-01 23:54:06 +0530

    hero animation implementation during flutter navigation

---
+++ chat/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import './screens/welcome_screen.dart';
+import './screens/login_screen.dart';
+import './screens/registration_screen.dart';
+import './screens/chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          // ignore: deprecated_member_use
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => WelcomeScreen(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
